d8e0c4b15e2e8cf3557b7bd3221c7696775fd34e
박준서 <jswing5267@naver.com>
2023-10-02 17:54:35 +0900

    10.10강

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { PageProps, graphql } from "gatsby";
 
-export default function Blog() {
+export default function Blog({ data }:PageProps<Queries.BlogTitlesQuery>) {
+  console.log(data);
   return (
     <Layout title="Blog">
-      <p>안녕 내 블로그에 온 걸 환영해!</p>
+      <ul>
+        {data.allFile.nodes.map((file,index) => <li key={index}>{file.name}</li>)}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
